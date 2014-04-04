356cf76dce4a3cdb66aba9e80fd2340980603382
Andy Wenk <andy@nms.de>
2014-04-04 23:31:11 +0200

    gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                dest: path.join(__dirname, ".grunt/gitbook"),
+                input: "./",
+                title: "Der Rytzendüsn vom Planeten Moklogg",
+                description: "Flug mit der Lichtrakete zur Erde",
+                github: "GitbookIO/javascript"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '.grunt/gitbook'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
